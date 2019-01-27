bfed91f0d03fcc38fd7c51b3462e7dbb524ef548
ArchambaultP <ehras@hotmail.com>
2019-01-26 19:00:39 -0500

    added seed values

--- db.js
+++ db.js
@@ -8,19 +8,19 @@ var con = mysql.createConnection({
 
 var accTable = `
 CREATE TABLE Accounts (
-	account_id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
     pass varchar(255) NOT NULL,
     first_name varchar(60) NOT NULL,
 	last_name varchar(60) NOT NULL,
-    phone_Number VARCHAR(25),
+    phone_number VARCHAR(25),
     address varchar(255),
     city varchar(255),
     postal_code varchar(8),
-    PRIMARY KEY ( account_id )
+    PRIMARY KEY ( id )
 );`
 
-var typeTable = `CREATE TABLE Type(
+var typeTable = `CREATE TABLE ProduceType(
 	id INT NOT NULL AUTO_INCREMENT,
     typeName varchar(255),
     primary key(id)
@@ -33,17 +33,28 @@ var prodTable = `CREATE TABLE Produce(
     isByWeight BOOLEAN NOT NULL,
     primary key (id),
     foreign key(type_id)
-    references Type(id)
+    references ProduceType(id)
 );`
 
 var listTable = `CREATE TABLE Listings(
 	id INT NOT NULL AUTO_INCREMENT,
     produce_id INT NOT NULL,
+    account_id INT NOT NULL,
     quantity INT NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY (produce_id)
-	REFERENCES Produce(id)
-);`
+	REFERENCES Produce(id),
+    FOREIGN KEY (account_id)
+    REFERENCES Accounts(id)
+);` 
+
+var seedAccount = `
+INSERT INTO Accounts (email,pass,first_name,last_name,phone_number,address,city,postal_code) VALUES ('admin','$2b$10$A7qVSDL.FrcLSnPeKJgapet8Cq24iD6c/5PZlb6Dwc3Fzuo7C5JeK','test','admin','450-531-6613','123 small street','Montreal', 'H4G 1S7')
+`
+
+var seedTypes = `
+INSERT INTO ProduceType (typeName) VALUES ('Vegetables'),('Fruit');
+`
 
 con.connect(function(err) {
     if (err) throw err;
@@ -60,6 +71,7 @@ con.connect(function(err) {
             con.query(accTable, function (err, result){
             if(err) throw err;
             console.log("Created Accounts !");
+
             con.query(typeTable, function (err, result){
                 if(err) throw err;
                 console.log("Created Types !");
@@ -69,7 +81,15 @@ con.connect(function(err) {
                     con.query(listTable, function (err, result){
                         if(err) throw err;
                         console.log("Created Listings !");
-                        
+                        con.query(seedAccount, function (err, result){
+                            if(err) throw err;
+                            console.log("Inserted Account !");
+                            con.query(seedTypes, function (err, result){
+                                if(err) throw err;
+                                console.log("Inserted Types !");
+                         
+                            });
+                        });
                     });
                     
                 });
